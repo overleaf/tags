bb4c1f1851042d7f8e0c1d43c7b0093cb2d1a805
Nate Stemen <nathanielstemen@gmail.com>
2020-02-13 11:21:37 -0500

    prettier: convert individual decaffeinated files to Prettier format

--- config/settings.defaults.js
+++ config/settings.defaults.js
@@ -1,21 +1,21 @@
-let Settings;
-module.exports = (Settings = {
-	internal: {
-		tags: {
-			port: 3012,
-			host: process.env["LISTEN_ADDRESS"] || "localhost"
-		}
-	},
+let Settings
+module.exports = Settings = {
+  internal: {
+    tags: {
+      port: 3012,
+      host: process.env.LISTEN_ADDRESS || 'localhost'
+    }
+  },
 
-	mongo: {
-		url: process.env['MONGO_CONNECTION_STRING'] || `mongodb://${process.env["MONGO_HOST"] || "localhost"}/sharelatex`
-	},
+  mongo: {
+    url:
+      process.env.MONGO_CONNECTION_STRING ||
+      `mongodb://${process.env.MONGO_HOST || 'localhost'}/sharelatex`
+  },
 
-	tags: {
-		healthCheck: {
-			user_id: "5620bece05509b0a7a3cbc62"
-		}
-	}
-});
-
-
+  tags: {
+    healthCheck: {
+      user_id: '5620bece05509b0a7a3cbc62'
+    }
+  }
+}
