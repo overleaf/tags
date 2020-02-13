dcffa810361de2e5c9eedcfb8a8f5e95dd3a4642
Nate Stemen <nathanielstemen@gmail.com>
2020-02-13 11:21:33 -0500

    prettier: convert test/acceptance decaffeinated files to Prettier format

--- test/acceptance/js/helpers/TagsApp.js
+++ test/acceptance/js/helpers/TagsApp.js
@@ -11,33 +11,41 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const app = require('../../../../app');
-const settings = require("settings-sharelatex");
+const app = require('../../../../app')
+const settings = require('settings-sharelatex')
 
 module.exports = {
-	running: false,
-	initing: false,
-	callbacks: [],
-	ensureRunning(callback) {
-		if (callback == null) { callback = function(error) {}; }
-		if (this.running) {
-			return callback();
-		} else if (this.initing) {
-			return this.callbacks.push(callback);
-		} else {
-			this.initing = true;
-			this.callbacks.push(callback);
-			return app.listen(settings.internal.tags.port, settings.internal.tags.host, error => {
-				if (error != null) { throw error; }
-				this.running = true;
-				return (() => {
-					const result = [];
-					for (callback of Array.from(this.callbacks)) {
-						result.push(callback());
-					}
-					return result;
-				})();
-			});
-		}
-	}
-};
+  running: false,
+  initing: false,
+  callbacks: [],
+  ensureRunning(callback) {
+    if (callback == null) {
+      callback = function(error) {}
+    }
+    if (this.running) {
+      return callback()
+    } else if (this.initing) {
+      return this.callbacks.push(callback)
+    } else {
+      this.initing = true
+      this.callbacks.push(callback)
+      return app.listen(
+        settings.internal.tags.port,
+        settings.internal.tags.host,
+        error => {
+          if (error != null) {
+            throw error
+          }
+          this.running = true
+          return (() => {
+            const result = []
+            for (callback of Array.from(this.callbacks)) {
+              result.push(callback())
+            }
+            return result
+          })()
+        }
+      )
+    }
+  }
+}
