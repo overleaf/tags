ad4d17cee6fcfdce3ca4b57e1012ee1a1c221682
Nate Stemen <nathanielstemen@gmail.com>
2020-02-13 11:21:35 -0500

    decaffeinate: convert individual files to js

--- app.js
+++ app.js
@@ -1,52 +1,63 @@
-metrics = require("metrics-sharelatex")
-metrics.initialize("tags")
-Settings = require 'settings-sharelatex'
-logger = require 'logger-sharelatex'
-logger.initialize("tags")
-express = require('express')
-app = express()
-controller = require("./app/js/TagsController")
-Path = require("path")
-metrics.memory.monitor(logger)
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS103: Rewrite code to no longer use __guard__
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const metrics = require("metrics-sharelatex");
+metrics.initialize("tags");
+const Settings = require('settings-sharelatex');
+const logger = require('logger-sharelatex');
+logger.initialize("tags");
+const express = require('express');
+const app = express();
+const controller = require("./app/js/TagsController");
+const Path = require("path");
+metrics.memory.monitor(logger);
 
-HealthCheckController = require("./app/js/HealthCheckController")
+const HealthCheckController = require("./app/js/HealthCheckController");
 
-app.configure ()->
-	app.use express.methodOverride()
-	app.use express.bodyParser()
-	app.use metrics.http.monitor(logger)
-	app.use express.errorHandler()
-metrics.injectMetricsRoute(app)
+app.configure(function(){
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(metrics.http.monitor(logger));
+	return app.use(express.errorHandler());
+});
+metrics.injectMetricsRoute(app);
 
-app.get  '/user/:user_id/tag', controller.getUserTags
-app.post '/user/:user_id/tag', controller.createTag
-app.put '/user/:user_id/tag', controller.updateTagUserIds
-app.post '/user/:user_id/tag/:tag_id/rename', controller.renameTag
-app.del  '/user/:user_id/tag/:tag_id', controller.deleteTag
-app.post '/user/:user_id/tag/:tag_id/project/:project_id', controller.addProjectToTag
-app.post '/user/:user_id/tag/project/:project_id', controller.addProjectToTagName
-app.del  '/user/:user_id/tag/:tag_id/project/:project_id', controller.removeProjectFromTag
-app.del  '/user/:user_id/project/:project_id', controller.removeProjectFromAllTags
+app.get('/user/:user_id/tag', controller.getUserTags);
+app.post('/user/:user_id/tag', controller.createTag);
+app.put('/user/:user_id/tag', controller.updateTagUserIds);
+app.post('/user/:user_id/tag/:tag_id/rename', controller.renameTag);
+app.del('/user/:user_id/tag/:tag_id', controller.deleteTag);
+app.post('/user/:user_id/tag/:tag_id/project/:project_id', controller.addProjectToTag);
+app.post('/user/:user_id/tag/project/:project_id', controller.addProjectToTagName);
+app.del('/user/:user_id/tag/:tag_id/project/:project_id', controller.removeProjectFromTag);
+app.del('/user/:user_id/project/:project_id', controller.removeProjectFromAllTags);
 
-app.get '/status', (req, res)->
-	res.send('tags sharelatex up')
+app.get('/status', (req, res) => res.send('tags sharelatex up'));
 
-app.get '/health_check', (req, res)->
-	HealthCheckController.check (err)->
-		if err?
-			logger.err err:err, "error performing health check"
-			res.send 500
-		else
-			res.send 200
+app.get('/health_check', (req, res) => HealthCheckController.check(function(err){
+    if (err != null) {
+        logger.err({err}, "error performing health check");
+        return res.send(500);
+    } else {
+        return res.send(200);
+    }
+}));
 
-app.get '*', (req, res)->
-	res.send 404
+app.get('*', (req, res) => res.send(404));
 
-host = Settings.internal?.tags?.host || "localhost"
-port = Settings.internal?.tags?.port || 3012
+const host = __guard__(Settings.internal != null ? Settings.internal.tags : undefined, x => x.host) || "localhost";
+const port = __guard__(Settings.internal != null ? Settings.internal.tags : undefined, x1 => x1.port) || 3012;
 
-module.exports = app
+module.exports = app;
 
-if !module.parent
-	app.listen port, host, ->
-		logger.info "tags starting up, listening on #{host}:#{port}"
+if (!module.parent) {
+	app.listen(port, host, () => logger.info(`tags starting up, listening on ${host}:${port}`));
+}
+
+function __guard__(value, transform) {
+  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
+}
--- config/settings.defaults.js
+++ config/settings.defaults.js
@@ -1,14 +1,21 @@
-module.exports = Settings =
-	internal:
-		tags:
-			port: 3012
-			host: process.env["LISTEN_ADDRESS"] or "localhost"
+let Settings;
+module.exports = (Settings = {
+	internal: {
+		tags: {
+			port: 3012,
+			host: process.env["LISTEN_ADDRESS"] || "localhost"
+		}
+	},
 
-	mongo:
-		url: process.env['MONGO_CONNECTION_STRING'] or "mongodb://#{process.env["MONGO_HOST"] or "localhost"}/sharelatex"
+	mongo: {
+		url: process.env['MONGO_CONNECTION_STRING'] || `mongodb://${process.env["MONGO_HOST"] || "localhost"}/sharelatex`
+	},
 
-	tags:
-		healthCheck:
+	tags: {
+		healthCheck: {
 			user_id: "5620bece05509b0a7a3cbc62"
+		}
+	}
+});
 
 
